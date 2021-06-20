f87956afe5055684e2d29a506d86f9a00073282a
Aygyun <49846915+achu94@users.noreply.github.com>
2021-06-20 03:51:30 +0200

    Update config.js

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const config = {
   PORT: process.env.PORT,
-  DB_URI: process.env.MONGODB_URI,
+  DB_URI: process.env.DB_URI,
   SALT_ROUNT: process.env.SALT_ROUNT,
   SECRET: process.env.SECRET,
   COOKIE_NAME: process.env.COOKIE_NAME
